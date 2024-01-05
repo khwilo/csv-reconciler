21b4a5950a989df047233b6d02141ef30abc5054
Khwilo Kabaka <khwilowatai@gmail.com>
2024-01-05 11:54:21 +0300

    feat: handle data transformations

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ def read_csv(file_path):
   with open(file_path, mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
-      data.append(row)
+      non_leading_space_row = {key: val.strip() for key, val in row.items()}
+      case_insensitive_row = {key: val.lower() for key, val in non_leading_space_row.items()}
+      data.append(case_insensitive_row)
   
   return data
 
 source_data = read_csv('source.csv')
 target_data = read_csv('target.csv')
-
